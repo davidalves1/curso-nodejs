357a4f625e30a7326b76042e720881c18114d7a3
davidalves1 <alves.david@outlook.com>
2016-11-23 22:35:19 -0200

    Aula 22

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,24 @@
 module.exports = function(app) {
-	app.get('/noticias', (require, response) => {
-		response.render('noticias/noticias')
+
+	app.get('/noticias', (req, res) => {
+
+		const mysql = require('mysql');
+
+		let databaseProfile = {
+			host: 'localhost',
+			database: 'portal_noticias',
+			user: 'root',
+			password: ''
+		};
+
+		let connection = mysql.createConnection(databaseProfile);
+
+		let query = 'SELECT * FROM noticias';
+
+		connection.query(query, (error, data) => {
+			res.send(data);
+		});
+	
+		// res.render('noticias/noticias')
 	});
 }

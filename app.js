ed04e9df5ee00fc235a0cd199d340533f6f26fec
davidalves1 <alves.david@outlook.com>
2016-11-24 23:14:24 -0200

    Aula 28

--- app.js
+++ app.js
@@ -2,15 +2,6 @@
 
 const app = require('./config/server');
 
-const home = require('./app/routes/home');
-home(app);
-
-const noticias = require('./app/routes/noticias');
-noticias(app);
-
-const add_noticia = require('./app/routes/add_noticia');
-add_noticia(app);
-
 app.listen(3000, () => {
 	console.log('Server listen on port 3000');
 });
--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const consign = require('consign');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -8,5 +9,6 @@ app.set('view engine', 'ejs');
 // O segundo parâmetro indica aonde deve buscar as views. 
 // Porém esse caminho parte de onde o express é instanciado.
 app.set('views', './app/views');
+consign().include('app/routes').into(app);
 
 module.exports = app;

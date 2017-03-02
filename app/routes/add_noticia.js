13b1bba9f052c838cff620368958a523e9ac6244
davidalves1 <alves.david@outlook.com>
2017-03-01 23:00:38 -0300

    Aula 35 - parcial

--- app/routes/add_noticia.js
+++ app/routes/add_noticia.js
@@ -2,4 +2,9 @@ module.exports = function(app) {
 	app.get('/add_noticia', (require, response) => {
 		response.render('admin/add_noticia')
 	});
+
+	app.get('/noticias/salvar', (require, response) => {
+		let noticias = req.body;
+		res.send(noticias);
+	});
 }
--- config/server.js
+++ config/server.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -9,6 +11,10 @@ app.set('view engine', 'ejs');
 // O segundo parâmetro indica aonde deve buscar as views. 
 // Porém esse caminho parte de onde o express é instanciado.
 app.set('views', './app/views');
+
+// Os middlewares devem ser inclu[idos antes do consign
+app.use(bodyParser.urlencoded({extended: true}));
+
 consign()
 	.include('app/routes')
 	.then('config/db_connection.js') // Tem que colocar a extensão, senão o consign confunde com um subdiretório
